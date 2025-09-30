c5dde660e0dc4670f7f63588a565bc4e1d2af0ef
MuhammadAhmadF2005 <istrikepro13ahmadfaisal@gmail.com>
2025-09-30 19:23:02 +0500

    leetcode folder made

---
+++ leetcode/leetcode108.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+using namespace std;
+int main()
+{
+
+    return 0;
+}
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *sortedArrayToBST(vector<int> &nums)
+    {
+    }
+};
