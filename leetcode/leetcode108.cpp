83189eafe9dbd2ccbdfce21c2fb002722f6481d4
MuhammadAhmadF2005 <istrikepro13ahmadfaisal@gmail.com>
2025-09-30 23:41:12 +0500

    leetcode 108 solved using recursion

--- leetcode/leetcode108.cpp
+++ leetcode/leetcode108.cpp
@@ -3,11 +3,6 @@
 #define vi vector<int>
 #define vll vector<long long>
 using namespace std;
-int main()
-{
-
-    return 0;
-}
 
 struct TreeNode
 {
@@ -24,5 +19,21 @@ class Solution
 public:
     TreeNode *sortedArrayToBST(vector<int> &nums)
     {
+        return helper(nums, 0, nums.size() - 1);
+    }
+
+private:
+    TreeNode *helper(vector<int> &nums, int l, int r)
+    {
+        if (l > r)
+            return nullptr;
+
+        int m = (l + r) / 2;
+        TreeNode *root = new TreeNode(nums[m]);
+
+        root->left = helper(nums, l, m - 1);
+        root->right = helper(nums, m + 1, r);
+
+        return root;
     }
 };
