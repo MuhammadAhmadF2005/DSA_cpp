c4a36b9d7961e03a79d712b5a67b6e1895db7942
MuhammadAhmadF2005 <istrikepro13ahmadfaisal@gmail.com>
2025-10-03 17:31:24 +0500

    leet 111 solved recursion still, not learned to implement using stacks yet

---
+++ leetcode/leetcode111.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+public:
+    int minDepth(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+
+        if (!root->left && !root->right)
+            return 1;
+
+        if (!root->left)
+        {
+            return 1 + minDepth(root->right);
+        }
+
+        if (!root->right)
+        {
+            return 1 + minDepth(root->left);
+        }
+
+        return (min(minDepth(root->left), minDepth(root->right)));
+    }
+};
