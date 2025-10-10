274ed07e7361f251b5e5a391afc15ebdef49ff26
MuhammadAhmadF2005 <istrikepro13ahmadfaisal@gmail.com>
2025-10-10 19:23:57 +0500

    PE 28 solved, en route to flawless 50

--- projects/CP/helloi.py
+++ projects/CP/helloi.py
@@ -1,28 +1,9 @@
-def isPrime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+total=1
+
+for k in range(1, 1001//2+1): 
+     total += 4 * (2*k + 1)**2 - 12*k
 
 
-max_primes = 0
-product = 0
+print(total)
 
-for a in range(-999, 1000):
-    for b in range(-1000, 1001):
-        n = 0
-        while True:
-            val = n**2 + a*n + b
-            if not isPrime(val):
-                break
-            n += 1
-
-        if n > max_primes:
-            max_primes = n
-            product = a * b
-
-print(product)
-
-
+    
