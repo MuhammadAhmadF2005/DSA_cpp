c6b887fc760b3e7b5f7a35fbf9d1990650fcf4e3
MuhammadAhmadF2005 <istrikepro13ahmadfaisal@gmail.com>
2025-10-23 01:16:47 +0500

    leet 20attempted

---
+++ leetcode/leetcode20.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        std::stack<char> stack;
+        std::unordered_map<char, char> closeToOpen = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}};
+
+        for (char c : s)
+        {
+            if (closeToOpen.count(c))
+            {
+                if (!stack.empty() && stack.top() == closeToOpen[c])
+                {
+                    stack.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                stack.push(c);
+            }
+        }
+        return stack.empty();
+    }
+};
