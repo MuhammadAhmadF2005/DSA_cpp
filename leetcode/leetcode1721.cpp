0071da3ff2d6a05bdcd1265a9c57ea19734a39d4
MuhammadAhmadF2005 <istrikepro13ahmadfaisal@gmail.com>
2025-10-30 18:50:45 +0500

    leet 1721 solved using simple val swapping

---
+++ leetcode/leetcode1721.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *swapNodes(ListNode *head, int k)
+    {
+        ListNode *curr = head;
+        int count = 0;
+        while (curr)
+        {
+            curr = curr->next;
+            count++;
+        }
+
+        int front = k;
+        int back = count - k;
+
+        curr = head;
+
+        for (int i = 1; i < front; i++)
+        {
+            curr = curr->next;
+        }
+        ListNode *first;
+
+        curr = head;
+        for (int i = 0; i < back; i++)
+        {
+            curr = curr->next;
+        }
+        ListNode *second = curr;
+
+        int temp = first->val;
+        first->val = second->val;
+        second->val = temp;
+    }
+};
