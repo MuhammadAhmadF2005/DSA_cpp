badcec51622eb9b55c7db6c567ef169166b84dc4
MuhammadAhmadF2005 <istrikepro13ahmadfaisal@gmail.com>
2025-11-02 14:08:19 +0500

    225 and 496 solved

---
+++ leetcode/leetcode225.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+using namespace std;
+int main()
+{
+
+    return 0;
+}
+
+class MyStack
+{
+public:
+    queue<int> q1, q2;
+    MyStack()
+    {
+    }
+
+    void push(int x)
+    {
+        q2.push(x);
+        while (!q1.empty())
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1, q2);
+    }
+
+    int pop()
+    {
+        int top = q1.front();
+        q1.pop();
+        return top;
+    }
+
+    int top()
+    {
+        return q1.front();
+    }
+
+    bool empty()
+    {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
