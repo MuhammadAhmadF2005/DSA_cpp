ffae7b5acf31b8949a6cafe61ec5df1816116f10
MuhammadAhmadF2005 <istrikepro13ahmadfaisal@gmail.com>
2025-11-10 18:13:29 +0500

    leetcode 222 done using recursive treetraversal call

---
+++ leetcode/leetcode222.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int countNodes(TreeNode *root)
+    {
+        TreeNode *left = nullptr;
+        TreeNode *right = nullptr;
+        int num = 0;
+        countNodes(TreeNode * left);
+        num++;
+        countNodes(TreeNode * right);
+    }
+};
--- leetcode/leetcode3321.cpp
+++ leetcode/leetcode3321.cpp
@@ -8,10 +8,13 @@ int main()
 
     return 0;
 }
+
+
 class Solution
 {
 public:
     vector<long long> findXSum(vector<int> &nums, int k, int x)
     {
+        
     }
 };
