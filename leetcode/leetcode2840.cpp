16faf6aeaa66249cb5d0a0e39f6bfe0838acde4e
MuhammadAhmadF2005 <istrikepro13ahmadfaisal@gmail.com>
2026-06-23 20:23:14 +0500

    leet 897 ! easy

---
+++ leetcode/leetcode2840.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+using namespace std;
+
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        for (int i = 0; i < s1.length(); i++)
+        {
+            
+        }
+        
+    }
+};
